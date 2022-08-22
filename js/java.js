2f0564f9d75fc39d47410a899979b2dbb7bbcdc9
Eduardo Santos <80988486+D1ng0ls@users.noreply.github.com>
2022-08-21 23:09:20 -0300

    projeto2

---
+++ js/java.js
@@ -0,0 +1,8 @@
+//CONVERSORES DE MASSA
+
+function mudacor() {
+    var cor = document.getElementById("cor").value;
+
+    document.getElementById("body").style.backgroundColor = cor;
+    document.getElementById("texto").style.color = cor;
+}
